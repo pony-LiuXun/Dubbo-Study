e1104e969ee47ede01971f05515ddce0d1c81066
liuxun <liuxun030792>
2019-03-08 16:21:34 +0800

    项目提交

---
+++ src/main/java/com/liuxun/dubbo/dubbostudy/DubboStudyApplication.java
@@ -0,0 +1,13 @@
+package com.liuxun.dubbo.dubbostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboStudyApplication.class, args);
+    }
+
+}
