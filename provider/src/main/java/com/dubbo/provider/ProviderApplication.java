9cc45939ed82102d8d6f8d74ee09d5115b90ac95
568875635@qq.com <568875635@qq.com>
2019-03-11 22:31:28 +0800

    微服务基础架构搭建一

--- provider/src/main/java/com/dubbo/provider/ProviderApplication.java
+++ provider/src/main/java/com/dubbo/provider/ProviderApplication.java
@@ -1,13 +1,13 @@
-package com.liuxun.dubbo.dubbostudy;
+package com.dubbo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DubboStudyApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboStudyApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
