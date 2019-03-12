f7a08f703070d7fb97a59b27221c914219e6e978
liuxun <liuxun030792>
2019-03-12 18:42:57 +0800

    问题修复

--- consumer/src/main/java/com/dubbo/consumer/controller/TestController.java
+++ consumer/src/main/java/com/dubbo/consumer/controller/TestController.java
@@ -1,6 +1,22 @@
 package com.dubbo.consumer.controller;
 
+import com.dubbo.service.TestSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
 public class TestController {
+
+    @Autowired
+    private TestSerivce testSerivce;
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "hello") String hello){
+        return testSerivce.hello(hello);
+    }
 
 
 }
--- provider/src/main/java/com/dubbo/provider/impl/TestService.java
+++ provider/src/main/java/com/dubbo/provider/impl/TestService.java
@@ -1,11 +1,13 @@
 package com.dubbo.provider.impl;
 
+import com.dubbo.service.TestSerivce;
 import org.springframework.stereotype.Service;
 
 @Service("testService")
-public class TestService implements com.dubbo.service.TestService {
+public class TestService implements TestSerivce {
+
     @Override
     public String hello(String str) {
-        return str+"你好！";
+        return "你好!"+str;
     }
 }
